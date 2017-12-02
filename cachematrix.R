81459c2703321f65ec0d427543a7fbbd79260b02
stevenvandorpe <33277427+stevenvandorpe@users.noreply.github.com>
2017-12-02 14:39:20 +1100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,30 @@
 
 ## Write a short comment describing this function
 
+testmatrix <- matrix(c(2,3,3,1,5,7,9,9,2), nrow=3, ncol=3)
+wrongmatrix <- matrix(c(2,2), nrow=2, ncol=1)
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
-
+oldmatrix = matrix(nrow=3, ncol=3)
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
+  if (nrow(x) == ncol(x)){
+    newmatrix <- x
+    if (newmatrix == oldmatrix){
+      print (inversem)
+    }
+    else{
+      oldmatrix <<- x
+      inversem <<- solve(x)
+      print(inversem)}
+  }
         ## Return a matrix that is the inverse of 'x'
+  else {print("Matrix must be invertible")}
 }
+
+
+cacheSolve(testmatrix)
